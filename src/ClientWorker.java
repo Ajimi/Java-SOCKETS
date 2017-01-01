de2acc329e9de7620a196bb15483b952f4cdb57e
ajimi <sloumma.21@gmail.com>
2017-01-01 14:42:14 +0100

    MultiThreaded

--- src/ClientWorker.java
+++ src/ClientWorker.java
@@ -1,15 +1,47 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by selim on 1/1/2017.
  */
-public class ClientWorker implements Runnable{
+public class ClientWorker implements Runnable {
     private Socket socket;
-    public ClientWorker(Socket socket){
+    DataInputStream inFromClient;
+    DataOutputStream outToClient;
+    public ClientWorker(Socket socket) {
         this.socket = socket;
+
     }
+
     @Override
     public void run() {
-        
+
+        try{
+            inFromClient = new DataInputStream(socket.getInputStream());
+
+            outToClient = new DataOutputStream(socket.getOutputStream());
+            while (true) {
+                String str = String.valueOf(inFromClient.readUTF());
+
+                System.out.println("The client :" + str);
+                Scanner sc = new Scanner(System.in);
+                str = sc.nextLine();
+                if (str.equals("quit")) {
+                    break;
+                }
+                outToClient.writeUTF(str);
+                outToClient.flush();
+
+            }
+            inFromClient.close();
+            outToClient.close();
+
+            socket.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,39 +1,18 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
 
         try {
             ServerSocket serverSocket = new ServerSocket(8855);
-            Socket socket = serverSocket.accept();
-            DataInputStream inFromClient;
-            DataOutputStream outToClient;
-
-            inFromClient = new DataInputStream(socket.getInputStream());
-
-            outToClient = new DataOutputStream(socket.getOutputStream());
             while (true) {
-                String str = String.valueOf(inFromClient.readUTF());
-
-                System.out.println("The client :" + str);
-                Scanner sc = new Scanner(System.in);
-                str = sc.nextLine();
-                if (str.equals("quit")) {
-                    break;
-                }
-                outToClient.writeUTF(str);
-                outToClient.flush();
-
+                Socket socket = serverSocket.accept();
+                ClientWorker worker = new ClientWorker(socket);
+                Thread t = new Thread(worker);
+                t.start();
             }
-            inFromClient.close();
-            outToClient.close();
-            serverSocket.close();
-            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
